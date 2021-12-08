12aba6ecc2fe31357dfac2c32f1f55f7e8cd603d
StarKing Develoeprs <88481114+sanultemp@users.noreply.github.com>
2021-12-08 10:56:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -37,8 +37,13 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('🎥 Flix Cinema LK', url='https://t.me/Flix_CinemaSL'),
-            InlineKeyboardButton('🎥 Flix PSA', url='https://t.me/FlixPSA')
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            ],[
+            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/EvaMariaUpdates')
+            ],[
+            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
+            InlineKeyboardButton('😊 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -72,11 +77,6 @@ async def start(client, message):
             )
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
-        buttons = [[
-            InlineKeyboardButton('🎥 Flix Cinema LK', url='https://t.me/Flix_CinemaSL'),
-            InlineKeyboardButton('🎥 Flix PSA', url='https://t.me/FlixPSA')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
@@ -106,6 +106,24 @@ async def start(client, message):
         caption=f_caption,
         )
                     
+@Client.on_message(filters.command("hide"))
+async def hide(client, message):
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id, message.from_user.first_name)
+        await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
+    if len(message.command) != 2:
+        buttons = [[
+            InlineKeyboardButton('🎥 Flix Cinema LK', url='https://t.me/Flix_CinemaSL'),
+            InlineKeyboardButton('🎥 Flix PSA', url='https://t.me/FlixPSA')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_photo(
+            photo=random.choice(PICS),
+            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
+        return
 
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
 async def channel_info(bot, message):
